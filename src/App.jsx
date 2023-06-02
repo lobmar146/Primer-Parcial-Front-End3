68a95572bfa4759a970d66047ae611846a7818e8
lobmar146 <lobmar146@gmail.com>
2023-06-02 11:46:17 -0300

    Subiendo modelo listo para rendir

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Listo para empezar a rendir baby</h1>
+    </>
+  )
+}
+
+export default App
